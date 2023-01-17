e6c67b4b2597e309047f810e7ae44dcf52a48c84
Adam <94019179+adam-abdelrahman0@users.noreply.github.com>
2023-01-17 10:49:35 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,22 +2,32 @@ let keys = ["c", "d", "e", "f", "g", "a", "b"];
 let currentKey = [];
 
 function getKey() {
+    keys = ["c", "d", "e", "f", "g", "a", "b"];
+    currentKey = [];
     let obj = document.getElementById("keySelect");
     let keyIndex = obj.options[obj.selectedIndex].text;
-    console.log(keys.indexOf(keyIndex.toLowerCase()));
+    keyIndex = keys.indexOf(keyIndex.toLowerCase());
 
-    for (let j = 0; j < 4; j++) {
-        console.log(j);
-        console.log(keys.shift());
-        
+    for (let j = 0; j < keyIndex; j++) {
+        currentKey.push(keys.shift());
     }
+
+    currentKey = keys.concat(currentKey);
+    currentKey = currentKey.map(k => k + "/4"); // convert to correct octaves
     console.log(currentKey);
 
-    keys = ["c", "d", "e", "f", "g", "a", "b"];
-    // Render voices.
-    voices.forEach(function (v) {
-        v.draw(context, stave);
-    });
+    placeNotes();
+
+}
+
+//formatter places mode notes on stave
+function placeNotes() {
+    notes = [];
+    for (let i = 0; i < currentKey.length; i++) {
+        notes.push(new Vex.Flow.StaveNote({keys: [currentKey[i]], duration: "w",}));
+    }
+
+    Vex.Flow.Formatter.FormatAndDraw(context, stave, notes);
 }
 
 const { Renderer, Stave } = Vex.Flow;
@@ -30,7 +40,7 @@ renderer.resize(600, 200);
 const context = renderer.getContext();
 
 // Create a stave of width 400 at position 10, 40 on the canvas.
-const stave = new Stave(10, 40, 500);
+let stave = new Stave(10, 40, 500);
 
 // Add a clef and time signature.
 stave.addClef("bass").addTimeSignature("4/4");
@@ -39,23 +49,6 @@ stave.addClef("bass").addTimeSignature("4/4");
 stave.setContext(context).draw();
 
 const sampleNotes = [keys[0] + "/4", "d/4", "e/4", "f/4", "g/4", "a/4", "b/4", "c/5"];
-const notes = [];
-
-for (let i = 0; i < sampleNotes.length; i++) {
-    notes.push(new Vex.Flow.StaveNote({keys: [sampleNotes[i]], duration: "w",}));
-}
-
-
-// Create a voice in 4/4 and add above notes
-const voices = [
-    new Vex.Flow.Voice({
-        num_beats: 8,
-        beat_value: 1,
-    }).addTickables(notes),
-
-];
-
-// Format and justify the notes to 400 pixels.
-new Vex.Flow.Formatter().joinVoices(voices).format(voices, 350);
+let notes = [];
 
 
